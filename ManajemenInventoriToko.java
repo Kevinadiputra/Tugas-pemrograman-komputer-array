edcf8ec675ed5fca36e4a935a55d02d2b87f8490
Randy Bachri <142553251+bachrirandy@users.noreply.github.com>
2023-11-03 23:52:32 +0700

    Add Java Source Code
    Add Java Source Code for Manajemen Inventori Toko Case Study

---
+++ ManajemenInventoriToko.java
@@ -0,0 +1,44 @@
+public class ManajemenInventoriToko {
+    public static void main(String[] args) {
+        String[][] inventory = {
+            {"Laptop", "8000000", "10"},
+            {"Smartphone", "3500000", "20"},
+            {"Tablet", "4100000", "10"},
+            {"Headphones", "800000", "30"},
+            {"Printer", "1200000", "14"}
+        };
+
+        // Menampilkan inventori awal
+        System.out.println("Inventori Toko:");
+        for (int i = 0; i < inventory.length; i++) {
+            String productName = inventory[i][0];
+            double productPrice = Double.parseDouble(inventory[i][1]);
+            int productStock = Integer.parseInt(inventory[i][2]);
+
+            System.out.println("Nama Produk: " + productName + ", Harga: " + productPrice + ", Stok: " + productStock);
+        }
+
+        // Menambahkan stok untuk "Laptop"
+        String[] ProdukTambahStok = {"Laptop", "Smartphone", "Tablet"};
+        int[] JumlahPenambahanStok = {20,20,40};
+        for (int i = 0; i < ProdukTambahStok.length; i++) {
+            for (int j = 0; j < inventory.length; j++) {
+                if (ProdukTambahStok[i].equals(inventory[j][0])) {
+                    int currentStock = Integer.parseInt(inventory[j][2]);
+                    inventory[j][2] = String.valueOf(currentStock + JumlahPenambahanStok[i]);
+                    break; // Produk ditemukan, keluar dari loop
+                }
+            }
+        }
+
+        // Menampilkan inventori setelah penambahan stok
+        System.out.println("\nInventori setelah penambahan stok:");
+        for (int i = 0; i < inventory.length; i++) {
+            String productName = inventory[i][0];
+            double productPrice = Double.parseDouble(inventory[i][1]);
+            int productStock = Integer.parseInt(inventory[i][2]);
+
+            System.out.println("Nama Produk: " + productName + ", Harga: " + productPrice + ", Stok: " + productStock);
+        }
+    }
+}
